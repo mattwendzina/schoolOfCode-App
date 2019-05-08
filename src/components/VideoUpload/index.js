a9a12954b75f7ebb580b85df5254dc71e6db0b57
jaskaran <jaskaran.sarkaria@googlemail.com>
2019-05-08 22:15:03 +0100

    debugging the video player

--- src/components/VideoUpload/index.js
+++ src/components/VideoUpload/index.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+const interviewQuestions = [
+  { question: "Tell us about yourself." },
+  { question: "Why do you want to learn to code?" },
+  { question: "What drives you?" },
+  { question: "Why do you want to join School of Code?" },
+  { question: "Explain something complex in simple terms." }
+];
+
+// bug you can't stop it from seperate places
+// bug can't keep stopping something that is already stopped errors at different points that call stop...
+// bug it actually starts recording if you pass stop twice
+// check video and audio are available, if not post alert.
+
 const VideoUpload = () => {
+  const [questionCounter, setQuestionCounter] = useState(0);
+  const [showVideo, setShowVideo] = useState(true);
   navigator.getUserMedia =
     navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
@@ -8,6 +24,7 @@ const VideoUpload = () => {
 
   let localStream;
   let recorder;
+
   // no microphone on this comp!!!!
   const constraints = {
     // audio: {
@@ -28,33 +45,50 @@ const VideoUpload = () => {
   };
 
   if (hasGetUserMedia()) {
+    console.log("features detected");
+  } else {
+    console.log("no feature detected");
+  }
+
+  const handleRecording = action => {
     try {
-      console.log("into the fire");
       navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-        localStream = stream;
-        stream.getTracks().forEach(function(track) {
-          console.log(track.getSettings()); //returns an object with all applicable constraints
-        });
-        recorder = new MediaRecorder(stream);
-        recorder.addEventListener("dataavailable", function(e) {
-          // e.data contains the audio data! let's associate it to an <audio> element
-          var el = document.querySelector("video");
-          el.src = URL.createObjectURL(e.data);
-        });
+        switch (action) {
+          case "stop":
+            var track = stream.getTracks()[0];
+            document.getElementById("videoUpload").pause();
+            document.getElementById("videoUpload").srcObject = null;
 
-        // start recording here...
-        recorder.start();
+            localStream.getTracks()[0].stop();
+            track.stop();
+            if (recorder.state === "recording") {
+              recorder.stop();
+            } else {
+              return;
+            }
+            return;
+          case "start":
+            document.getElementById("videoUpload").play();
+            localStream = stream;
+            recorder = new MediaRecorder(stream);
+            recorder.addEventListener("dataavailable", function(e) {
+              // e.data contains the audio data! let's associate it to an <audio> element
 
-        return (document.getElementById("videoUpload").srcObject = stream);
+              var el = document.querySelector("video");
+              el.src = URL.createObjectURL(e.data);
+            });
 
-        // here we are feeding the video tag the mediaStram from the webcam
+            recorder.start();
+            return (document.getElementById("videoUpload").srcObject = stream);
+
+          default:
+            return;
+        }
       });
     } catch (err) {
       console.error(err);
     }
-  } else {
-    console.log("no feature detected");
-  }
+  };
 
   // getUserMedia() can only be called from an HTTPS URL, localhost or a file:// URL.
   // Otherwise, the promise from the call will be rejected. getUserMedia() also won't work for cross origin
@@ -62,87 +96,69 @@ const VideoUpload = () => {
 
   return (
     <div style={{ textAlign: "center" }}>
-      <video
-        controls
-        autoPlay
-        id="videoUpload"
-        height="50%"
-        width="80%"
-        display="block"
-      />
-      <br />
-      <>
-        <button
-          onClick={() => {
-            document.getElementById("videoUpload").pause();
-          }}
-        >
-          Pause Recording
-        </button>
-        <button
-          onClick={() => {
-            try {
-              navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-                var track = stream.getTracks()[0];
-                document.getElementById("videoUpload").pause();
-                document.getElementById("videoUpload").srcObject = null;
-                localStream.getTracks()[0].stop();
-                track.stop();
-                recorder.stop();
-              });
-            } catch (err) {
-              console.error(err);
-            }
-          }}
-        >
-          Stop Recording
-        </button>
-        <button
-          onClick={() => {
-            try {
-              navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-                document.getElementById("videoUpload").play();
-                localStream = stream;
-                return (document.getElementById(
-                  "videoUpload"
-                ).srcObject = stream);
-                // here we are feeding the video tag the mediaStram from the webcam
-              });
-            } catch (err) {
-              console.log(err);
-            }
-          }}
-        >
-          Resume Recording
-        </button>
-        <button
-          onClick={() => {
-            try {
-              navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-                document.getElementById("videoUpload").play();
-                localStream = stream;
-                recorder = new MediaRecorder(stream);
-                recorder.addEventListener("dataavailable", function(e) {
-                  // e.data contains the audio data! let's associate it to an <audio> element
-                  var el = document.querySelector("video");
-                  el.src = URL.createObjectURL(e.data);
-                });
-
-                recorder.start();
-                return (document.getElementById(
-                  "videoUpload"
-                ).srcObject = stream);
-
-                // here we are feeding the video tag the mediaStram from the webcam
-              });
-            } catch (err) {
-              console.log(err);
-            }
-          }}
-        >
-          Restart Recording
-        </button>
-      </>
+      {showVideo &&
+        interviewQuestions
+          .slice(
+            questionCounter,
+            questionCounter + 1 > interviewQuestions.length
+              ? null
+              : questionCounter + 1
+          )
+          // could cause problem when it gets to the end of the list
+          .map(item => {
+            return (
+              <>
+                <h1>
+                  {item.question} {questionCounter + 1}/5
+                </h1>
+                <video
+                  controls
+                  autoPlay
+                  id="videoUpload"
+                  height="50%"
+                  width="80%"
+                  display="block"
+                />
+                <br />
+                <button
+                  onClick={() => {
+                    handleRecording("start");
+                  }}
+                >
+                  Start Recording
+                </button>
+                <>
+                  <button
+                    onClick={() => {
+                      handleRecording("stop");
+                    }}
+                  >
+                    Stop Recording
+                  </button>
+                </>
+                {questionCounter + 1 < interviewQuestions.length ? (
+                  <button
+                    onClick={() => {
+                      // handleRecording("stop");
+                      setQuestionCounter(questionCounter + 1);
+                      // upload to datbase;
+                    }}
+                  >
+                    Next Question
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      // handleRecording("stop");
+                      setShowVideo(false);
+                    }}
+                  >
+                    Sumbit Final
+                  </button>
+                )}
+              </>
+            );
+          })}
     </div>
   );
 };
