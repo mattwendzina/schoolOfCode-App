5755277978c5a613d667ad232c6414e7b50b6354
Aidan Walker <aidanw549@gmail.com>
2019-05-21 12:51:11 +0100

    form page 1 finished

--- src/components/ApplicationFormPage1/index.js
+++ src/components/ApplicationFormPage1/index.js
@@ -59,40 +59,67 @@ const FormPart1 = ({
                 alt="School of code logo"
               />
             </div>
-          </div>
-          <div className={css.rightContainer}>
-            <div className={css.formContainer}>
-              <h1 style={{ color: "white" }}>Step 1 of 5</h1>
-              {/* <div className={css.instructionsContainer} /> */}
-
-              <div className={css.firstNameContainer}>
-                <label className={css.firstNameLabel}>First Name</label>
-                <div className={css.firstNameInputContainer}>
-                  <input
-                    placeholder="Enter first name.."
-                    className={css.firstName}
-                    type="text"
-                    name="firstName"
-                    onChange={updateField}
-                    defaultValue={formValues.firstName}
-                    required
-                  />
-                </div>
+            <div className={css.instructionsContainer}>
+              <h2 className={css.instructionsText}>Instructions</h2>
+              <br />
+              <p className={css.instructionsParagraph}>
+                Please fill out steps 1-5 of the application form so we can get
+                to know more about you. Once you have submitted the information
+                you will receive and email which will notify you if you have
+                successfully made it to the next process.
+              </p>
+            </div>
+            <div className={css.questionBoxContainer}>
+              <div className={css.selectdiv}>
+                <label>
+                  <select>
+                    <option selected>-- Questions --</option>
+                    <option>Step 1 of 5</option>
+                    <option>Step 2 of 5</option>
+                    <option>Step 3 of 5</option>
+                    <option>Step 4 of 5</option>
+                    <option>Step 5 of 5</option>
+                  </select>
+                </label>
               </div>
+            </div>
+          </div>
 
-              <div className={css.lastNameContainer}>
-                <label className={css.lastNameLabel}>Last Name</label>
+          <div className={css.rightContainer}>
+            <h1 className={css.applicationFormText}>Application Form</h1>
+            <h1 className={css.applicationStepText}>Step 1</h1>
+            <div className={css.formContainer}>
+              {/* <div className={css.instructionsContainer} /> */}
+              <div className={css.firstAndLastNameContainer}>
+                <div className={css.firstNameContainer}>
+                  <label className={css.firstNameLabel}>First Name</label>
+                  <div className={css.firstNameInputContainer}>
+                    <input
+                      placeholder="Enter first name.."
+                      className={css.firstName}
+                      type="text"
+                      name="firstName"
+                      onChange={updateField}
+                      defaultValue={formValues.firstName}
+                      required
+                    />
+                  </div>
+                </div>
 
-                <div className={css.lastNameInputContainer}>
-                  <input
-                    placeholder="Enter Last name.."
-                    className={css.lastName}
-                    type="text"
-                    name="lastName"
-                    onChange={updateField}
-                    defaultValue={formValues.lastName}
-                    required
-                  />
+                <div className={css.lastNameContainer}>
+                  <label className={css.lastNameLabel}>Last Name</label>
+
+                  <div className={css.lastNameInputContainer}>
+                    <input
+                      placeholder="Enter last name.."
+                      className={css.lastName}
+                      type="text"
+                      name="lastName"
+                      onChange={updateField}
+                      defaultValue={formValues.lastName}
+                      required
+                    />
+                  </div>
                 </div>
               </div>
               <div className={css.emailContainer}>
@@ -118,12 +145,12 @@ const FormPart1 = ({
                 {" "}
                 {formError.emailError}
               </div>
-              <div className={css.nextButton}>
-                <button onClick={saveAndContinue} className={css.nextInput}>
-                  {" "}
-                  Next{" "}
-                </button>
-              </div>
+            </div>
+            <div className={css.nextButton}>
+              <button onClick={saveAndContinue} className={css.nextInput}>
+                {" "}
+                Next{" "}
+              </button>
             </div>
           </div>
         </div>
