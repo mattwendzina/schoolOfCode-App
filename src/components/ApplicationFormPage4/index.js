55c093771ebbc4faf42d6e5486929067c29bfbc3
Aidan Walker <aidanw549@gmail.com>
2019-05-21 14:42:18 +0100

    afternoon css updates all form pages setup, 3-5 need styling

--- src/components/ApplicationFormPage4/index.js
+++ src/components/ApplicationFormPage4/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-import css from "../ApplicationFormPage4/ApplicationFormPage4.module.css";
+import css from "../ApplicationFormPage3/ApplicationFormPage3.module.css";
+import SocImage from "../../Images/soc-logo.png";
+
 const FormPart4 = ({
   step,
   setStep,
@@ -43,36 +45,74 @@ const FormPart4 = ({
 
   return (
     <>
-      <div className={css.header}>
-        <h1>Application Form</h1> <h1>Step 4 of 5</h1>
-      </div>
       <div className={css.wrapper}>
-        <div className={css.formContainer}>
-          <div className={css.question}>
-            <label>
-              Please explain why you would like to do the School of Code
-            </label>
+        <div className={css.container}>
+          <div className={css.leftContainer}>
+            <div className={css.socImageContainer}>
+              <img
+                src={SocImage}
+                className={css.socImage}
+                alt="School of code logo"
+              />
+            </div>
+            <div className={css.instructionsContainer}>
+              <h2 className={css.instructionsText}>Instructions</h2>
+              <br />
+              <p className={css.instructionsParagraph}>
+                Please fill out steps 1-5 of the application form so we can get
+                to know more about you. Once you have submitted the information
+                you will receive and email which will notify you if you have
+                successfully made it to the next process.
+              </p>
+            </div>
+            <div className={css.questionBoxContainer}>
+              <div className={css.selectdiv}>
+                <label>
+                  <select>
+                    <option selected>-- Questions --</option>
+                    <option>Step 1 of 5</option>
+                    <option>Step 2 of 5</option>
+                    <option>Step 3 of 5</option>
+                    <option>Step 4 of 5</option>
+                    <option>Step 5 of 5</option>
+                  </select>
+                </label>
+              </div>
+            </div>
           </div>
-          <input
-            className={css.input}
-            type="text"
-            name="motivationQuestion"
-            onChange={updateField}
-            defaultValue={formValues.motivationQuestion}
-          />
-          <div style={{ color: "red", fontSize: 12 }}>
-            {formError.inputError}
+          <div className={css.rightContainer}>
+            <h1 className={css.applicationFormText}>Application Form</h1>
+            <h1 className={css.applicationStepText}>Step 4</h1>
+            <div className={css.formContainer}>
+              <div className={css.question}>
+                <label>
+                  Please explain why you would like to do the School of Code
+                </label>
+              </div>
+              <input
+                className={css.input}
+                type="text"
+                name="motivationQuestion"
+                onChange={updateField}
+                defaultValue={formValues.motivationQuestion}
+              />
+              <div style={{ color: "red", fontSize: 12 }}>
+                {formError.inputError}
+              </div>
+            </div>
+            <div className={css.previousAndNextContainer}>
+              <button onClick={previous} className={css.previousButton}>
+                {" "}
+                Previous{" "}
+              </button>
+              <div className={css.nextButtonContainer}>
+                <button onClick={review} className={css.nextButton}>
+                  {" "}
+                  Next{" "}
+                </button>
+              </div>
+            </div>
           </div>
-
-          <button onClick={previous} className={css.previousButton}>
-            {" "}
-            Previous{" "}
-          </button>
-
-          <button onClick={review} className={css.reviewButton}>
-            {" "}
-            Review{" "}
-          </button>
         </div>
       </div>
     </>
--- src/components/ApplicationFormReviewPage/index.js
+++ src/components/ApplicationFormReviewPage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import css from "../ApplicationFormReviewPage/ApplicationFormReviewPage.module.css";
+import SocImage from "../../Images/soc-logo.png";
 const ReviewForm = ({ step, setStep, formValues, submitForm }) => {
   const previous = e => {
     e.preventDefault();
@@ -7,27 +8,61 @@ const ReviewForm = ({ step, setStep, formValues, submitForm }) => {
   };
   return (
     <>
-      <div className={css.header}>
-        <h1>Application Form</h1> <h1>Step 5 of 5</h1>
-      </div>
-      <h2>Review Information</h2>
       <div className={css.wrapper}>
-        <div className={css.formContainer}>
-          <ul>
-            {Object.keys(formValues).map(item => (
-              <li>{formValues[item]}</li>
-            ))}
-          </ul>
-
-          <div className={css.buttons}>
-            <button onClick={previous} className={css.previousButton}>
-              {" "}
-              Previous{" "}
-            </button>
-            <button onClick={submitForm} className={css.submitButton}>
-              {" "}
-              Submit{" "}
-            </button>
+        <div className={css.container}>
+          <div className={css.leftContainer}>
+            <div className={css.socImageContainer}>
+              <img
+                src={SocImage}
+                className={css.socImage}
+                alt="School of code logo"
+              />
+            </div>
+            <div className={css.instructionsContainer}>
+              <h2 className={css.instructionsText}>Instructions</h2>
+              <br />
+              <p className={css.instructionsParagraph}>
+                Please fill out steps 1-5 of the application form so we can get
+                to know more about you. Once you have submitted the information
+                you will receive and email which will notify you if you have
+                successfully made it to the next process.
+              </p>
+            </div>
+            <div className={css.questionBoxContainer}>
+              <div className={css.selectdiv}>
+                <label>
+                  <select>
+                    <option selected>-- Questions --</option>
+                    <option>Step 1 of 5</option>
+                    <option>Step 2 of 5</option>
+                    <option>Step 3 of 5</option>
+                    <option>Step 4 of 5</option>
+                    <option>Step 5 of 5</option>
+                  </select>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className={css.rightContainer}>
+            <h1 className={css.applicationFormText}>Application Form</h1>
+            <h1 className={css.applicationStepText}>Step 5</h1>
+            <div className={css.formContainer}>
+              <ul>
+                {Object.keys(formValues).map(item => (
+                  <li>{formValues[item]}</li>
+                ))}
+              </ul>
+            </div>
+            <div className={css.buttons}>
+              <button onClick={previous} className={css.previousButton}>
+                {" "}
+                Previous{" "}
+              </button>
+              <button onClick={submitForm} className={css.submitButton}>
+                {" "}
+                Submit{" "}
+              </button>
+            </div>
           </div>
         </div>
       </div>
